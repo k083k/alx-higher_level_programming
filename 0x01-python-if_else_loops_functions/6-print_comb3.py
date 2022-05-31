ac6cec5f772b80f408c7fac293cf970e17fb44e0
k083k <kobekore@gmail.com>
2022-05-31 06:02:22 -0700

    Task 6 Finished

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 9):
     for x in range(i + 1, 10):
         if not (i == 8 and x == 9):
-            print("{i:d}{x:d}".format(i, x), end=", ")
+            print("{:d}{:d}".format(i, x), end=", ")
         else:
             print("{:d}{:d}".format(i, x), end="\n")
