b5eec3bdf1bff432efa4abaa4c9404f6ae4a2cc2
k083k <kobekore@gmail.com>
2022-06-02 15:29:20 -0700

    Advanced Tasks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,11 +13,11 @@ if __name__ == "__main__":
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-        if sys.argv[2] == "+":
+        if sys.argv[2] is "+":
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif sys.argv[2] == "-":
+        elif sys.argv[2] is "-":
             print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif sys.argv[2] == "*":
+        elif sys.argv[2] is "*":
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif sys.argv[2] == "/":
+        elif sys.argv[2] is "/":
             print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
