08db3abd9fe3862c3ff682723d007fb3d99b3ecc
k083k <kobekore@gmail.com>
2022-06-22 02:35:09 -0700

    Task 5 Corrected

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,38 +1,33 @@
 #!/usr/bin/python3
+"""Defines a class square."""
 
 
 class Square:
+    """Class square with private attribute instance"""
+    def __init__(self, size=0):
+        """Ïnitialize a new Square with size argument"""
+        self.size = size
+
+    def area(self):
+        """"Return the current Square area"""
+        return (self.__size * self.__size)
 
     @property
     def size(self):
-        return(self.__size)
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
+        self.__size = value
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-
-    def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-
-    def area(self):
-        return (self.__size ** 2)
-
 
     def my_print(self):
+        """Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
         if self.__size == 0:
             print("")
-        else:
-            for i in range(0, self.__size):
-                for j in range(0, self.__size):
-                    print('#', end="")
-                print()
