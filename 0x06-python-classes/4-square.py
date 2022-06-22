5f5152d6c1e85a957bbc51c11bf36e836b46d6f3
k083k <kobekore@gmail.com>
2022-06-22 02:34:21 -0700

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
+"""Defines a class square."""
 
 
 class Square:
+    """Class square with private attribute instance"""
     def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        """Ïnitialize a new Square with size argument"""
+        self.size = size
+
+    def area(self):
+        """"Return the current Square area"""
+        return (self.__size * self.__size)
 
     @property
     def size(self):
-        return(self.__size)
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
+        self.__size = value
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-
-    def area(self):
-        return(self.__size ** 2)
