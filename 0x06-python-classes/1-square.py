9cc65be8f4d6834fb0572134165ad2c1ee59fcea
k083k <kobekore@gmail.com>
2022-06-22 02:31:53 -0700

    Task 1 Done

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Defines a class square based on 0-square.py"""
+
 
 class Square:
-
+    """Class square with private attribute instance, size"""
     def __init__(self, size):
+        """Ïnitialize a new Square with size argument"""
         self.__size = size
