8e303353275db0a4deffdb87305bba45de9f9a03
k083k <kobekore@gmail.com>
2022-06-29 02:24:36 -0700

    task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-"""
-function that prints my First Name and Last Name
-"""
+"""Defines a name-printing function."""
+
 
 def say_my_name(first_name, last_name=""):
-    """
-    function that prints full name.
+    """Print a name.
+
     Args:
-        first_name: string
-        last_name: string
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
     Raises:
-        TypeError:
-            first_name must be a string
-            last_name must be a string
-
-    Returns:
-        <first name> <last name>
+        TypeError: If either of first_name or last_name are not strings.
     """
-    if type(first_name) != str:
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    elif type(last_name) != str:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
     print("My name is {} {}".format(first_name, last_name))
