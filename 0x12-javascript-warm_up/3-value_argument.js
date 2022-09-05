ed70ced6a45dc62796c2d5c0360f9090f153acf9
k083k <kobekore@gmail.com>
2022-09-05 05:37:02 -0700

    Task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-if (argv[2] === undefined){
-	console.log('No argument');
-}else{
+if (argv[2]){
 	console.log(argv[2]);
+}else{
+	console.log('No argument');
 }
