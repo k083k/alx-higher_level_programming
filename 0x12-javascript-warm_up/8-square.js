873b36b09b4614d66cf960398e1d9de1360936b9
k083k <kobekore@gmail.com>
2022-09-05 05:39:24 -0700

    Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-arg = process.argv[2];
-const x = Number(arg);
-if (!arg || isNaN(arg)){
-	console.log('Missing size')
-} else if (arg >= 1){
-	let s = '';
-	for (i = 0; i < arg; i++){
-		s += 'X';
-	}
-	for(i = 0; i < x; i++){
-		console.log(s);
-	}
+const x = Number(process.argv[2]);
+if (isNaN(x)) {
+  console.log('Missing size');
+} else {
+  let i;
+  let s = '';
+  for (i = 0; i < x; i++) {
+    s += 'X';
+  }
+  for (i = 0; i < x; i++) {
+    console.log(s);
+  }
 }
