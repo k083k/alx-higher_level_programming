3e3c511b1d0ac0d39fd754f2c02577106df54de6
k083k <kobekore@gmail.com>
2022-09-05 05:47:59 -0700

    Advanced Task

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,13 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function (value) {
+/*
+YOUR CODE HERE
+*/
+myObject.incr = function () {
   this.value++;
 };
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
